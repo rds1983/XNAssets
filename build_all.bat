45a522ac0b02d6347d6c48441587868ef0d0b7e3
Roman Shapiro <eleroman@mail.ru>
2020-02-22 18:58:53 +0700

    Build simplification

--- build_all.bat
+++ build_all.bat
@@ -1,6 +1,3 @@
 dotnet --version
 dotnet build build\XNAssets.MonoGame.sln /p:Configuration=Release --no-incremental
 dotnet build build\XNAssets.Xenko.sln /p:Configuration=Release --no-incremental
-call copy_zip_package_files.bat
-rename "ZipPackage" "XNAssets.%APPVEYOR_BUILD_VERSION%"
-7z a XNAssets.%APPVEYOR_BUILD_VERSION%.zip XNAssets.%APPVEYOR_BUILD_VERSION%
