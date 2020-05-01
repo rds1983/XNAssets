fedc9ee019af880dd3229619663440ac4c4e88ef
Roman Shapiro <roman.shapiro@grapecity.com>
2020-05-01 18:47:55 +0700

    Build fix

--- build_all.bat
+++ build_all.bat
@@ -1,3 +1,3 @@
 dotnet --version
 dotnet build build\XNAssets.MonoGame.sln /p:Configuration=Release --no-incremental
-dotnet build build\XNAssets.Xenko.sln /p:Configuration=Release --no-incremental
+dotnet build build\XNAssets.Stride.sln /p:Configuration=Release --no-incremental
